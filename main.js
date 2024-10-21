22626954f0c3eef97e61286d4619ec48054879e4
Raj Shekhar <rajshekhar111201@gmail.com>
2024-10-21 23:36:46 +0530

    PORT TO ENV

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const empModel = require("./models");
 const db = require("./db");
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 const routerImport = require("./employeeRoute");
 app.use("/employee", routerImport);
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
 
 //all running
-
